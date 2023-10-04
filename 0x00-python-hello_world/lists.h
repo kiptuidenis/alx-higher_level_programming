01ac23e7c8f5d2a557eeb537a6bad21bebad500c
kiptuidenis <kiptuidenis@gmail.com>
2023-10-04 23:31:29 +0300

    Task 10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,16 +3,23 @@
 
 #include <stdlib.h>
 
-/* Define the structure for a singly linked list node */
-struct listint_t {
-    int data;
-    struct listint_t *next;
-};
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-
-typedef struct listint_t listint_t;
-
-
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
