64b38832a538d30e485b7e00588b386c8672ea9d
kiptuidenis <kiptuidenis@gmail.com>
2023-10-05 18:43:39 +0300

    Technical Interview Practice

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,52 +3,38 @@
 #include "lists.h"
 
 /**
- * insert_node - inserts a number into a sorted singly linked list.
- * @head: points to head of list
- * @number: number to be inserted
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: Pointer to the head of the list
+ * @number: Number to be inserted
  *
- * Return: pointer to inserted number
+ * Return: Pointer to the inserted number
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *ahead;
-	listint_t *behind;
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *current = *head;
+	listint_t *previous = NULL;
 
-	behind = *head;
-	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	if (*head == NULL)
+
+	new->n = number;
+
+	if (*head == NULL || (*head)->n >= number)
 	{
+		new->next = *head;
 		*head = new;
-		new->n = number;
 		return (new);
 	}
-	if (behind->n >= number)
+
+	while (current != NULL && current->n < number)
 	{
-		*head = new;
-		new->next = behind;
-		new->n = number;
-		return (new);
+		previous = current;
+		current = current->next;
 	}
-	else
-	{
-		ahead = behind->next;
-		while (ahead != NULL)
-		{
-			if (ahead->n >= number)
-			{
-				new->next = ahead;
-				new->n = number;
-				behind->next = new;
-				return (new);
-			}
-			ahead = ahead->next;
-			behind = behind->next;
-		}
-		new = add_nodeint_end(head, number);
-		return (new);
-	}
+
+	new->next = current;
+	previous->next = new;
+
+	return (new);
 }
