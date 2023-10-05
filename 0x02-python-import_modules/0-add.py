fb8882f97051033865fb238cf6c8e3c694e68b5e
kiptuidenis <kiptuidenis@gmail.com>
2023-10-05 12:38:49 +0300

    Update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
-from add_0 import add
-
-
-def add_nums():
-    a = 1
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-    return
-
 if __name__ == "__main__":
-    add_nums()
+    from add_0 import add
+    def add_nums():
+        a = 1
+        b = 2
+
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        return
