18700f354afdeead47ec19ead1a2d863ed370556
kiptuidenis <kiptuidenis@gmail.com>
2023-10-05 12:56:39 +0300

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-def printArgs():
     length = len(argv)
 
     if length == 1:
         print("{} arguments.".format(0))
-        return
+        exit()
 
     elif length == 2:
         print("{} argument:".format(length - 1))
@@ -16,8 +16,3 @@ def printArgs():
 
     for args in range(1, length):
          print("{}: {} ".format(args, argv[args]))
-
-    return
-
-if __name__ == "__main__":
-    printArgs()
