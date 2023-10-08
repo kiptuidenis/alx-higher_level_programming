c1d06108d3bc5308868ca49aa4dfa356ecdbab47
kiptuidenis <kiptuidenis@gmail.com>
2023-10-08 12:06:18 +0300

    Technical interview prep

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -50,3 +50,26 @@ int is_palindrome(listint_t **head)
 	}
 	return (is_palindrome);
 }
+
+
+
+/**
+ * reverse_list - reverses a singly linked list
+ * @head: double pointer to the head of the list
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
--- 0x03-python-data_structures/lists.c
+++ 0x03-python-data_structures/lists.c
@@ -72,25 +72,3 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
-
-
-/**
- * reverse_list - reverses a singly linked list
- * @head: double pointer to the head of the list
- */
-void reverse_list(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
