f620cb713f984eab3bac4dbf75ef5c37f2fd597a
kiptuidenis <kiptuidenis@gmail.com>
2023-10-11 14:56:24 +0300

    Task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """searches and replaces elements"""
+
+    new_list = [x if x != search else replace for x in my_list]
+    return new_list
