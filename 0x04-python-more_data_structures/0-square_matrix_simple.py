4198a51e4d768717e7aa99230dddc45de29e66f9
kiptuidenis <kiptuidenis@gmail.com>
2023-10-11 14:46:21 +0300

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     """computes square values of all integers"""
-    if not matrix:
-        return
-
     new_list = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
     return new_list
