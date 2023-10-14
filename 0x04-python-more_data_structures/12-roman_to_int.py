3f113ca47686a1827a8cc54a3fddea6f3855d657
kiptuidenis <kiptuidenis@gmail.com>
2023-10-14 21:42:12 +0300

    changes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,53 +1,22 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+
     if not roman_string:
         return 0
-    greater = 0
-    smaller = 0
-    roman_list = []
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    if len(roman_string) == 1:
-        if roman_string[0] not in romans:
-            return 0
-        for key, value in romans.items():
-            if roman_string[0] == key:
-                return value
+    prev_value = 0  # Initialize the previous value
 
-    for letter in roman_string:
+    for letter in roman_string[::-1]:  # Reverse from right to left
         if letter not in romans:
             return 0
-        for key, value in romans.items():
-            if letter == key:
-                roman_list.append(value)
-                break
-
-    for i in range(0, len(roman_list)):
-        j = i + 1
-        if j == len(roman_list) - 1:
-            if roman_list[i] > roman_list[j]:
-                greater = greater + roman_list[i] + roman_list[j]
-            if roman_list[i] < roman_list[j]:
-                smaller = smaller + roman_list[i]
-                greater = greater + roman_list[j]
-            if roman_list[i] == roman_list[j]:
-                if (roman_list[i] == 5 or roman_list[i] == 50
-                or roman_list[i] == 500):
-                    return 0
-                greater = greater + roman_list[i] + roman_list[j]
-            break
-
+        value = romans[letter]
+        if value < prev_value:
+            result -= value  # Subtract the value if it's smaller than the prev
         else:
-            if roman_list[i] > roman_list[j]:
-                greater = greater + roman_list[i]
-            elif roman_list[i] < roman_list[j]:
-                smaller = smaller + roman_list[i]
-            elif roman_list[i] == roman_list[j]:
-                if (roman_list[i] == 5 or roman_list[i] == 50
-                or roman_list[i] == 500):
-                    return 0
-                else:
-                    greater = greater + roman_list[i]
-    result = greater - smaller
+            result += value  # Add the value if it's greater or equal to
+        prev_value = value  # Update the previous value for the next iteration
+
     return result
