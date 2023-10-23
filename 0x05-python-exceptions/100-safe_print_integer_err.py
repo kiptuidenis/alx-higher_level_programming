524dc4e97d8195514a2e61ec311a0c800470a1ee
kiptuidenis <kiptuidenis@gmail.com>
2023-10-23 16:51:21 +0300

    Update

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """prints an integer"""
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(str(e)), file=sys.stderr)
+        return False
