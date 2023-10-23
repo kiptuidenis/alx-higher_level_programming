040cfc6d78bad9e76d7ee93911ae21e2cac2704c
kiptuidenis <kiptuidenis@gmail.com>
2023-10-23 12:15:49 +0300

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,7 @@
 
 def safe_print_list(my_list=[], x=0):
     """Safely prints the contents of a list"""
+    i = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
