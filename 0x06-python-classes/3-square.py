f29c10a1eca8237a4304fe339d7b1f29a5a521d4
kiptuidenis <kiptuidenis@gmail.com>
2023-10-24 18:16:09 +0300

    Update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,6 +5,8 @@
 class Square:
     """This is a Square class that has attribute size with conditions"""
     def __init__(self, size=0):
+        """Initializes an instance of Square"""
+
         if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")
@@ -12,5 +14,8 @@ class Square:
                 self.__size = size
         else:
             raise TypeError("size must be an integer")
+
     def area(self):
+        """Calculates the area of square instance"""
+
         return self.__size * self.__size
