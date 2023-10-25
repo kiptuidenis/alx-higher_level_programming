d4f2d3bc561e19cec6fe4662ef4f8811a2fd3ee4
kiptuidenis <kiptuidenis@gmail.com>
2023-10-25 11:28:11 +0300

    Update

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if isinstance(value, int):
+            self.__data = value
+        else:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if isinstance(value, Node) or value is None:
+            self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        
+        if self.__head is None or self.__head.data >= value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        result = "SinglyLinkedList:\n"
+        current = self.__head
+        while current:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result
+
+
--- 0x06-python-classes/5-main.py
+++ 0x06-python-classes/5-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 Square = __import__('5-square').Square
 
-my_square = Square(3)
+my_square = Square(-1)
 my_square.my_print()
 
 print("--")
 
-my_square.size = 10
+my_square.size = 5
 my_square.my_print()
 
 print("--")
