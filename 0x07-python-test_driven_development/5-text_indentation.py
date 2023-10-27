dcf13a61508ee233ab5ffb18caac2ec45f32a140
kiptuidenis <kiptuidenis@gmail.com>
2023-10-27 09:30:29 +0300

    Update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module prints the given text
+"""
+
+
+def text_indentation(text):
+    """This function prints the text based on the following conditions:
+    text must be a string, otherwise raise a TypeError exception with
+    the message text must be a string
+    There should be no space at the beginning or at the end of each printed line
+
+    Args: text
+    """
+    if not isinstance(text, str):
+        raise TypeError("Input must be a string")
+    i = 0
+
+    while(i != len(text) - 1):
+        if text[i] in ('.', '?', ':'):
+            print(text[i])
+            print()
+
+            if text[i + 1] in (' '):
+                i += 2
+        else:
+            print(text[i], end="")
+            i += 1
