ff59b0e3b39ebb79af7bf726c85d26f2fe945924
kiptuidenis <kiptuidenis@gmail.com>
2023-11-01 14:52:09 +0300

    Updates

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,15 +14,26 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         """ This is the test function
         """
-        list = [8, 200, 40, 1000, 20]
-        self.assertIsNone(max_integer())
-        self.assertEqual(max_integer(list), 1000)
+        self.assertEqual(max_integer([8, 200, 40, 1000, 20]), 1000)
 
+    def test_none_args(self):
+        """Tests for no arguments"""
+        self.assertIsNone(max_integer())
+
+    def test_none_list_args(self):
+        """Tests if argument is a list"""
         with self.assertRaises(TypeError):
             max_integer(7)
-            max_integer([7, 8, 'o', 77])
-            max_integer([9, 3, 5], [4, 5, 5])
 
+    def test_list_not_homogenous(self):
+        """Test if all elements in list are integers"""
+        with self.assertRaises(TypeError):
+            max_integer([7, 8, 'o', 77])
+
+    def test_list_not_homog(self):
+        """Test if all elements in lists"""
+        with self.assertRaises(TypeError):
+            max_integer([9, 3, 5], [4, 5, 5])
 
 if __name__ == '__main__':
     unittest.main()
