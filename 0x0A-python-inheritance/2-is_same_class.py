6bd178b809bfa59a0b249e6a64aa1dcc3f748cbd
kiptuidenis <kiptuidenis@gmail.com>
2023-11-06 15:10:21 +0300

    Updates

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains a function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an 
+    instance of the specified class ; otherwise False
+
+    Args:
+        obj : Object to be checked
+        a_class : Class to be checked against
+    """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
