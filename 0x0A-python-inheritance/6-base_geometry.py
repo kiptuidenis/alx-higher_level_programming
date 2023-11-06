420262d139fa2846e15752e0601ac382051fe4cc
kiptuidenis <kiptuidenis@gmail.com>
2023-11-06 15:36:14 +0300

    Updates

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Raises a simple error
+"""
+
+
+class BaseGeometry():
+    """Raises an exception error
+    """
+    def area(self):
+        """Raises an exception error
+        """
+        raise Exception("area() is not implemented")
