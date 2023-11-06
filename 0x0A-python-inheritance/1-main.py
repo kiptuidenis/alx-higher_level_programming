b1d6585d44f5e6f03fce1ca59124ef11d764c64e
kiptuidenis <kiptuidenis@gmail.com>
2023-11-06 14:45:51 +0300

    Updates

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -4,7 +4,7 @@ MyList = __import__('1-my_list').MyList
 my_list = MyList()
 my_list.append(1)
 my_list.append(4)
-my_list.append(3.99)
+my_list.append("ty")
 my_list.append(3.9)
 my_list.append(5)
 print(my_list)
