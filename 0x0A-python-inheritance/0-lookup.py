3e0daa4571321895959ba4e1589dd3c0a2360fa3
kiptuidenis <kiptuidenis@gmail.com>
2023-11-06 12:50:24 +0300

    Updates

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Returns The list of available attributes and methods"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+
+    return dir(obj)
