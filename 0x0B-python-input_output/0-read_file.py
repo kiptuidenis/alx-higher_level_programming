1fc5a6ff4c4dccbe4d405626b9d22c96ef1bd530
kiptuidenis <kiptuidenis@gmail.com>
2023-11-07 12:27:06 +0300

    Updates

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     Args:
         filename (str, optional): Name of fie. Defaults to "".
     """
-    with open(filename, 'r', encoding='utf8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
