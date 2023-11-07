3bfae88a4c378b188c436e2a9077a58b0097883c
kiptuidenis <kiptuidenis@gmail.com>
2023-11-07 17:02:39 +0300

    Updates

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -16,7 +16,6 @@ def class_to_json(obj):
     """
     dir_list = dir(obj)
     fields = [name for name in dir_list if not callable(getattr(obj, name)) and not name.startswith("__")]
-    print(fields)
     attr_dict = {fields[i]: getattr(obj, fields[i]) 
     for i in range(0, len(fields))}
     return attr_dict
