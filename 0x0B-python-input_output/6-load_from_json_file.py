a3ffd3f4be520e42c44461444f56e04908ab9710
kiptuidenis <kiptuidenis@gmail.com>
2023-11-07 11:36:36 +0300

    Updates

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates an Object from JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a “JSON file”:
+
+    Args:
+        filename (string): Name of JSON file
+    """
+    with open(filename, 'r', encoding='utf8') as f:
+        return json.loads(f.read())
+    
