092fe8b3f6c10ba75666ac81a8cbc45ed6c87532
kiptuidenis <kiptuidenis@gmail.com>
2023-11-29 16:55:55 +0300

    Updates

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""This module contains the Rectangle class
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """Defines the dimensions of the rectangle among other parameters
+
+    Args:
+        Base (class): Handles the 'id' attribute
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """This is the constructor of this class
+
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+            x (int, optional): Defaults to 0.
+            y (int, optional): Defaults to 0.
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter method of width attribute
+        """
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Setter method of width attribute
+
+        Args:
+            value (int): 
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method of height attribute
+        """
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """Setter method of height attribute
+
+        Args:
+            value (int): 
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method of x attribute
+        """
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """Setter method of x attribute
+
+        Args:
+            value (int): 
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method of y attribute
+        """
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """Setter method of y attribute
+
+        Args:
+            value (int): 
+        """
+        self.__y = value
