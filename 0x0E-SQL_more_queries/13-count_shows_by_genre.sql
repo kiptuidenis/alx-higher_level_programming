7310577fc5a4da9b1b57ddef92a6b381bdba88b0
kiptuidenis <kiptuidenis@gmail.com>
2023-12-23 09:39:06 +0300

    Updates

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- lists all rows of a database meeting a condition
 SELECT tv_genres.name, count(*) AS number_of_shows
 FROM tv_show_genres 
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
