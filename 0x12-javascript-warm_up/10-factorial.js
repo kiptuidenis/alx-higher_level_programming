7229647953ef5acd73c654c0ea1ff122a529903c
kiptuidenis <kiptuidenis@gmail.com>
2024-01-09 14:24:32 +0300

    Updates

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 let a = Number(process.argv[2]);
+console.log(a);
 console.log(fact(a));
 function fact(a){
-    if (a === 0){
-        return 1;
-    }
-    if (a === NaN){
+    if (a === 0 || a === NaN){
         return 1;
     }
     return a * fact(a - 1);
