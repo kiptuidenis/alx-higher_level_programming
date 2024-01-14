b7f7d8558ecf8f006a7c50ef1bc1b95f37a9582f
kiptuidenis <kiptuidenis@gmail.com>
2024-01-14 15:45:44 +0300

    Updates

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Creates a table named states with id and name as columns"""
 
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import Column, String, Integer, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 database_url = 'mysql+mysqlconnector://denis:8904Jack@localhost:3306/hbtn_0e_6_usa'
 
 class State(Base):
@@ -17,8 +17,8 @@ class State(Base):
     """
     __tablename__ = "states"
 
-    id = Column("id", Integer, default=11, primary_key=True, unique=True, autoincrement=True, nullable=False)
-    name = Column("name", String(128), nullable=False)
+    id = Column(Integer, default=11, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
     def __init__(self, id, name):
         """Constructor to initialize name and id
@@ -30,4 +30,3 @@ class State(Base):
         self.id = id
         self.name = name
 
-engine = create_engine(database_url, echo=True)
