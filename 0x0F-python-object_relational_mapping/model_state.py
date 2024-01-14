814b73de7b97189d6ff24e95bdff42e2a32c548d
kiptuidenis <kiptuidenis@gmail.com>
2024-01-14 16:04:00 +0300

    Updates

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -19,14 +19,3 @@ class State(Base):
 
     id = Column(Integer, default=11, primary_key=True, unique=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    def __init__(self, id, name):
-        """Constructor to initialize name and id
-
-        Args:
-            id (int): id of the state
-            name (str): name of the state
-        """
-        self.id = id
-        self.name = name
-
