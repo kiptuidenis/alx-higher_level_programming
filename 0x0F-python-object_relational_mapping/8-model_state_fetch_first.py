976e970ad00fd74fda21299dce62aa6a245fabf3
kiptuidenis <kiptuidenis@gmail.com>
2024-01-14 18:32:15 +0300

    Updates

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Prints the first State object from the database"""
+
+import sys
+from sqlalchemy import create_engine, select
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # Define the database URL
+    database_url = 'mysql+mysqldb://{}:{}@localhost/{}'.\
+    format(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    #create engine
+    engine = create_engine(database_url)
+
+    #create session
+    session = Session(engine)
+
+    #specify columns you want to select
+    columns = [State.id, State.name]
+
+    #build a select statement
+    query = select(*columns).where(State.id == 1)
+
+    #Execute query and fetch the results
+    rows = session.execute(query).fetchall()
+
+    #Display the results
+    for row in rows:
+        print("{}: {}".format(row.id, row.name))
