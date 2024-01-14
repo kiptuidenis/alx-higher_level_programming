c9ca2377335168b5437091c269ad5f23c4b1b20e
kiptuidenis <kiptuidenis@gmail.com>
2024-01-14 22:59:15 +0300

    Updates

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,7 +10,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     database_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.\
         format(sys.argv[1], sys.argv[2], sys.argv[3])
-    
+
     engine = create_engine(database_url)
     session = Session(engine)
 
