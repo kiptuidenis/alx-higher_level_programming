fc3cb132adafa63832080104d92220e1b1f221e2
kiptuidenis <kiptuidenis@gmail.com>
2024-03-24 12:35:36 +0300

    Revision

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """Replaces all occurences of an element by another in a new list"""
+    new_list = [replace if x == search else x for x in my_list]
+    return new_list
