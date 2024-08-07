66ce42c97c207dcdfef68bbea5b3c55ccb294670
kiptuidenis <kiptuidenis@gmail.com>
2024-08-07 14:44:15 +0300

    Changes

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-if (!Number(process.argv[2])) {
-    console.log("Not a number");
-} else {
-    console.log("My number:", process.argv[2])
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-let myLangs = ["C is fun", "Python is cool", "JavaScript is amazing"];
-myLangs.forEach(function(item) {
-    console.log(item);
-});
