3f21412376ff410812ade99f0e8567366613e871
kiptuidenis <kiptuidenis@gmail.com>
2025-03-10 09:58:24 +0300

    Removed bugs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
+            printed_elements += 1
+        print()
     except IndexError:
-        pass
-    return i + 1
+        print()
+    return printed_elements
