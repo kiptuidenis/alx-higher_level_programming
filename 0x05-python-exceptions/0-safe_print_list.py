401cc8f2b3ea41257c655dcafa90583b39bc6487
kiptuidenis <kiptuidenis@gmail.com>
2025-03-10 09:43:04 +0300

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i])
+    except IndexError:
+        pass
+    return i + 1
