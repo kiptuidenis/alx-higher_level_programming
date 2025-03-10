3c5510b1cba08ff1481679a91941cff5b1e56502
kiptuidenis <kiptuidenis@gmail.com>
2025-03-10 09:47:04 +0300

    Correction made

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i])
+            print(my_list[i], end="")
     except IndexError:
         pass
     return i + 1
