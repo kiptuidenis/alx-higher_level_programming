a6d9d5b8b2106c6b5fb7da6200c1c5c016add985
kiptuidenis <kiptuidenis@gmail.com>
2025-03-10 14:41:30 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+""""Task 2: Square with size"""
+
+class Square:
+    """This is a class Square that
+    defines a square by size"""
+    def __init__(self, size):
+        """Initializes the size of the square"""
+        self.__size = size
+    
