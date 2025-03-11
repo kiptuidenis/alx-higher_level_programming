f701d6da825acd9c32b12d328568ec4d19919ab7
kiptuidenis <kiptuidenis@gmail.com>
2025-03-11 09:36:04 +0300

    Task 0

--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,1 +1,1 @@
-This directory contains tasks on import and modules
+Tasks on import modules
---
+++ 0x02-python-import_modules/fibo_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Unit tests for fibo.py"""
+
+from fibo import fib
+
+def test_fib():
+    fib(1000)
+
+test_fib()
